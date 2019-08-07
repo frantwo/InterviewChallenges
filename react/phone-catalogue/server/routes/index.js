1f1379e28f319450d6d7d301ac012a3aac53f3a9
Francho Garcia <franchog79@gmail.com>
2019-08-07 21:41:59 +0200

    Endpoint finished and server port in 5000. Cors intalled

--- react/phone-catalogue/server/routes/index.js
+++ react/phone-catalogue/server/routes/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const data = require("../public/phones.json");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/phones", (req, res, next) => {
+  res.json(data);
 });
 
 module.exports = router;
